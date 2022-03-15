51a579eeceb4584be73946e0fd5c40be4e592c0c
FeryazHacimuradov <tu201906125@code.edu.az>
2022-03-15 17:34:33 +0400

    addedSize

---
+++ Fenco/Areas/admin/Controllers/SizesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fenco.Data;
+using Fenco.Models;
+
+namespace Fenco.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class SizesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SizesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: admin/Sizes
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Sizes.Include(s => s.ParentSize);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: admin/Sizes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _context.Sizes
+                .Include(s => s.ParentSize)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+
+            return View(size);
+        }
+
+        // GET: admin/Sizes/Create
+        public IActionResult Create()
+        {
+            ViewData["ParentSizeId"] = new SelectList(_context.Sizes, "Id", "Name");
+            return View();
+        }
+
+        // POST: admin/Sizes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ParentSizeId")] Size size)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(size);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentSizeId"] = new SelectList(_context.Sizes, "Id", "Name", size.ParentSizeId);
+            return View(size);
+        }
+
+        // GET: admin/Sizes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _context.Sizes.FindAsync(id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+            ViewData["ParentSizeId"] = new SelectList(_context.Sizes, "Id", "Name", size.ParentSizeId);
+            return View(size);
+        }
+
+        // POST: admin/Sizes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ParentSizeId")] Size size)
+        {
+            if (id != size.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(size);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SizeExists(size.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentSizeId"] = new SelectList(_context.Sizes, "Id", "Name", size.ParentSizeId);
+            return View(size);
+        }
+
+        // GET: admin/Sizes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _context.Sizes
+                .Include(s => s.ParentSize)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+
+            return View(size);
+        }
+
+        // POST: admin/Sizes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var size = await _context.Sizes.FindAsync(id);
+            _context.Sizes.Remove(size);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SizeExists(int id)
+        {
+            return _context.Sizes.Any(e => e.Id == id);
+        }
+    }
+}
